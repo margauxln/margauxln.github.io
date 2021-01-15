89a7a678fd5c4f8d5431afe993ca0785294e2e50
Margaux Lenne <margaux.lennebonety@gmail.com>
2021-01-15 15:42:45 +0100

    ajout imageLoaded

---
+++ imagesloaded.js
@@ -0,0 +1,31 @@
+// init Masonry
+$(function(){
+    console.log("page chargee!")
+    var container= document.querySelector('.grid');
+    var msnry = new Masonry( container, {"itemSelector": ".grid-item", "columnWidth": ".grid-item","percentPosition": "true"});
+//   // layout Masonry after each image loads
+//     container.imagesLoaded().progress( function() {
+//     console.log("Nouvelle Image chargee")
+//     msnry.layout();
+//     });  
+var imgLoad = imagesLoaded($('.grid'));
+imgLoad.on( 'progress', function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    msnry.layout();
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+});
+
+//   .done( function( instance ) {
+//     console.log('all images successfully loaded');
+//   })
+//   .fail( function() {
+//     console.log('all images loaded, at least one is broken');
+//   })
+//   .progress( function( instance, image ) {
+//     var result = image.isLoaded ? 'loaded' : 'broken';
+//     console.log( 'image is ' + result + ' for ' + image.img.src );
+//   });
+
+})
+
+
