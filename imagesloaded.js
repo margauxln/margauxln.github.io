2d91995530f15ed75d540b40c11c877dc7b82e21
Margaux Lenne <margaux.lennebonety@gmail.com>
2021-02-02 17:00:00 +0100

    cv

--- imagesloaded.js
+++ imagesloaded.js
@@ -3,6 +3,11 @@ $(function(){
     console.log("page chargee!")
     var container= document.querySelector('.grid');
     var msnry = new Masonry( container, {"itemSelector": ".grid-item", "columnWidth": ".grid-item","percentPosition": "true"});
+    // <!-- data-masonry='{ ""columnWidth": ".grid-sizer"": ".grid-sizer", "itemSelector": ".grid-item", "percentPosition": "true" }'
+    var list = document.getElementsByClassName('grid');
+    for (i=0;i<list.length;i++){
+        list[i].setAttribute('data-masonry', '{ ""columnWidth": ".grid-sizer"", "itemSelector": ".grid-item", "percentPosition": "true" }');
+    }
     var imgLoad = imagesLoaded($('.grid'));
     imgLoad.on( 'progress', function( instance, image ) {
         var result = image.isLoaded ? 'loaded' : 'broken';
